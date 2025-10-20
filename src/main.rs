b20ae62823c7e8f599c3956927a28dd4e861d42d
edamame <edamame0@proton.me>
2025-10-19 22:15:16 -0700

    Basic send data included

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,21 @@
 use std::collections::HashSet;
 use std::convert::Infallible;
 use std::io::{self, Write};
+use std::fs;
 use std::net::IpAddr;
 use std::sync::LazyLock;
-use std::time::{Duration, Instant, UNIX_EPOCH};
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
 use anyhow::Result;
 use base64::engine::{general_purpose::STANDARD, Engine};
-use clap::{Id, Parser};
+use clap::{Parser};
 use futures_util::future::BoxFuture;
-use futures_util::stream::Skip;
 use futures_util::FutureExt;
-use http::{header, HeaderMap, HeaderName, HeaderValue};
+use http::{HeaderMap, HeaderName, HeaderValue};
 use libsignal_core::curve::{KeyPair, PublicKey};
+use libsignal_core::{DeviceId, ProtocolAddress};
 use libsignal_net::chat::server_requests::ServerEvent;
-use libsignal_net::chat::ws::{self, Chat, ListenerEvent, Responder};
+use libsignal_net::chat::ws::{ListenerEvent};
 use libsignal_net::chat::{ChatConnection, ChatHeaders, ConnectError, LanguageList, Request};
 use libsignal_net::connect_state::{
     ConnectState, ConnectionResources, DefaultConnectorFactory, SUGGESTED_CONNECT_CONFIG,
@@ -23,21 +24,18 @@ use libsignal_net::connect_state::{
 use libsignal_net::env::{UserAgent, PROD};
 use libsignal_net::infra::dns::DnsResolver;
 use libsignal_net::infra::route::{
-    ConnectionProxyConfig, ConnectionProxyRoute, DirectOrProxyProvider, DirectOrProxyRoute,
+    ConnectionProxyRoute, DirectOrProxyProvider, DirectOrProxyRoute,
     PreconnectingFactory, SimpleRoute, TcpRoute, TlsRouteFragment,
 };
 use libsignal_net::infra::tcp_ssl::TcpSslConnector;
 use libsignal_net::infra::{EnableDomainFronting, EnforceMinimumTls};
-use libsignal_net::proto::chat_websocket::WebSocketRequestMessage;
 use libsignal_net_chat::api::ChallengeOption;
 use libsignal_net_chat::api::{registration::*, Unauth};
 use libsignal_net_chat::registration::{ConnectUnauthChat, RegistrationService};
-use libsignal_net_grpc::proto::chat::account::ConfirmUsernameHashRequest;
-use libsignal_protocol::{kem, sealed_sender_encrypt, KyberPreKeyRecord};
-use libsignal_protocol::{GenericSignedPreKey, IdentityKeyPair, InMemSignalProtocolStore};
-use messaging::{decrypt_message, encrypt_message};
+use libsignal_protocol::{sealed_sender_decrypt, sealed_sender_encrypt, IdentityKeyStore, SenderCertificate, ServerCertificate, Timestamp};
+use libsignal_protocol::{IdentityKeyPair, InMemSignalProtocolStore};
 use rand_core::{OsRng, TryRngCore};
-use serde::{ser, Deserialize, Serialize};
+use serde::{Deserialize, Serialize};
 use tokio::sync::oneshot;
 use zkgroup::profiles::ProfileKey;
 
@@ -174,7 +172,7 @@ async fn main() -> Result<()> {
     let registration_id = rng.random::<u16>() & 0x3FFF;
     let pni_registration_id = rng.random::<u16>() & 0x3FFF;
 
-    registration_service.register_account(
+    let _ = registration_service.register_account(
         NewMessageNotification::WillFetchMessages,
         ProvidedAccountAttributes {
             recovery_password: &recovery_password,
@@ -191,11 +189,13 @@ async fn main() -> Result<()> {
         Some(SkipDeviceTransfer),
         ForServiceIds::generate(|k| keys.get(k).as_borrowed()),
         &str::from_utf8(&password).unwrap(),
-    );
+    ).await;
 
     // TODO: Would have to submit all the data to register the account. Make sure keys are
     // serialized before doing this!
     // registration_service.register_account(message_notification, account_attributes, device_transfer, keys, account_password)
+
+    let network_change_event = SENDER_THAT_NEVER_SENDS.subscribe();
 
     let connector = ProductionChatConnector {
         user_agent: UserAgent::with_libsignal_version("TODO"),
@@ -211,34 +211,70 @@ async fn main() -> Result<()> {
     // which is a method on the Chat type, so we need to get a "Chat", which wraps the connection
     // to the server.
     // TODO: figure out what to pass for on_disconnect
-    let chat = connector.connect_chat(on_disconnect).await?;
-    chat.send(
+
+    let (tx, rx) = oneshot::channel();
+    let chat = connector.connect_chat(tx).await?;
+
+    let request_path = "/v1/message";
+    let test_ptext = vec![1, 2, 3, 23, 99]; // TODO: get real text here
+    let mut store = InMemSignalProtocolStore::new(IdentityKeyPair::generate(&mut rng), rng.random::<u32>())?;
+
+    let trust_root = KeyPair::generate(&mut rng);
+    let server_key = KeyPair::generate(&mut rng);
+    let server_cert = ServerCertificate::new(1, server_key.public_key, &trust_root.private_key, &mut rng)?;
+
+    let sender_cert = SenderCertificate::new(
+        "9d0652a3-dcc3-4d11-975f-74d61598733f".to_string().clone(),
+        Some("+12345678901".to_owned().clone()),
+        *store.get_identity_key_pair().await?.public_key(),
+        DeviceId::new(23).unwrap(),
+        Timestamp::from_epoch_millis(SystemTime::now().duration_since(UNIX_EPOCH)?.as_millis() as u64),
+        server_cert,
+        &server_key.private_key,
+        &mut rng,
+    )?;
+
+    let body = sealed_sender_encrypt(
+        // TODO: figure out what these have to be
+        &ProtocolAddress::new("796abedb-ca4e-4f18-8803-1fde5b921f9f".to_string().clone(), DeviceId::new(42).unwrap()),
+        &sender_cert,
+        &test_ptext,
+        &mut store.session_store,
+        &mut store.identity_store,
+        SystemTime::now(),
+        &mut rng,
+    ).await;
+
+    let signal_timestamp = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_millis().to_string();
+
+    let res = chat.send(
         Request {
             method: http::Method::POST,
-            path: "/v1/message",
+            path: request_path.parse().unwrap(),
             headers: HeaderMap::from_iter([
                 (
-                    HeaderName::from("X-Signal-Key"),
-                    HeaderValue::from_str("false"),
+                    HeaderName::from_static("x-signal-key"),
+                    HeaderValue::from_static("false"),
                 ),
                 (
-                    HeaderName::from("X-Signal-Timestamp"),
-                    HeaderValue::from_str(Instant::now().duration_since(UNIX_EPOCH).as_millis()),
+                    HeaderName::from_static("x-signal-timestamp"),
+                    HeaderValue::from_str(&signal_timestamp.as_str())?,
                 ),
             ]),
-            body: sealed_sender_encrypt(
-                // TODO: figure out what these have to be
-                destination,
-                sender_cert,
-                ptext,
-                session_store,
-                identity_store,
-                now,
-                rng,
-            ),
+            body: match body.into() {
+                Ok(byte_vec) => Some(byte_vec.into()),
+                Err(_) => None
+            },
         },
-        timeout,
-    );
+        Duration::from_secs(3),
+    ).await?;
+
+    println!("{:?}", res);
+
+    match rx.await {
+        Ok(v) => println!("got = {:?}", v),
+        Err(_) => println!("the sender dropped"),
+    }
 
     return Ok(());
 }
@@ -337,15 +373,15 @@ impl ConnectUnauthChat for ProductionChatConnector {
                     let listener = move |event| match event {
                         ListenerEvent::Finished(_) => drop(on_disconnect.take()),
                         ListenerEvent::ReceivedAlerts(_) | ListenerEvent::ReceivedMessage(_, _) => {
-                            let event: Result<ServerEvent, _> = event.try_into().unwrap();
-                            let (addr, msg) = decrypt_message(
-                                ciphertext,
-                                trust_root,
-                                local_uuid,
-                                local_device_id,
-                                store,
-                                rng,
-                            );
+                            // let event: Result<ServerEvent, _> = event.try_into().unwrap();
+                            // let (addr, msg) = decrypt_message(
+                            //     ciphertext,
+                            //     trust_root,
+                            //     local_uuid,
+                            //     local_device_id,
+                            //     store,
+                            //     rng,
+                            // );
                             // TODO: do something with the encrypted message
                         }
                     };
@@ -514,5 +550,9 @@ fn read_user_input(prompt: &str) -> Result<String> {
     io::stdout().flush()?;
     let mut input = String::new();
     io::stdin().read_line(&mut input)?;
-    Ok(input.trim().to_string())
+    let mut res = input.trim().to_string();
+    if &res[..5] == "FILE:" {
+        res = fs::read_to_string(&res[5..]).expect("Should have been able to read the file");
+    }
+    return Ok(res);
 }
